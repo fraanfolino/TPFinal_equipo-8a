ff548efeeb1c7185ba8a6b7daa782c00f040d436
fraanfolino <fran_fol@hotmail.com>
2025-05-29 12:54:25 -0300

    Agregado de nuevas clases

--- Dominio/Producto.cs
+++ Dominio/Producto.cs
@@ -6,7 +6,11 @@ using System.Threading.Tasks;
 
 namespace Dominio
 {
-    internal class Producto
+    public class Producto
     {
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+        public decimal Precio { get; set; }
+        public int Stock { get; set; }
     }
 }
--- Dominio/Transaccion.cs
+++ Dominio/Transaccion.cs
@@ -6,7 +6,11 @@ using System.Threading.Tasks;
 
 namespace Dominio
 {
-    internal class Transaccion
+    public class Transaccion
     {
+        public int Id { get; set; }
+        public List<Producto> Productos { get; set; }
+        public DateTime FechaPedido { get; set; }
+        public decimal Total { get; set; }
     }
 }
